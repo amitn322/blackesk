8bcf9e58a4920dc431fa1f8c6e3dee2a2582014e
Amit Nepal <amit@amitnepal.com>
2022-01-06 00:27:36 +0000

    Syslog-ng Permission Denied Fixed and updated all to latest version

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker-compose -f docker-compose-multinode.yml down && docker volume rm es_certs es_data01 es_data02 es_data03
-
+docker ps | grep -i 'es02\|es03'
+if [ $? -eq 0 ];then
+        echo "Multi node setup detected. Uninstalling.."
+        docker-compose -f docker-compose-multinode.yml down && docker volume rm es_certs es_data01 es_data02 es_data03
+else
+        echo "Single Node setup detected, Uninstalling.."
+        docker-compose -f docker-compose-singlenode.yml down && docker volume rm es_certs es_data01
+fi
+echo "(-) Removing Syslog ng image"
+docker rmi es_syslog-ng
+echo "(-) Removing Creds file"
+rm .creds.txt
+echo "Uninstall Complete"
